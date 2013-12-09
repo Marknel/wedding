991f1fef78d3dca6a724850d05ee8aab4382217d
Mark Cracknell <mark@kondoot.com>
2013-12-09 23:53:33 +1000

    Improving the invite show page with some better styling and functionality

--- app/assets/javascripts/invites.js
+++ app/assets/javascripts/invites.js
@@ -2,22 +2,72 @@
 // All this logic will automatically be available in application.js.
 
 jQuery(document).ready(function ($) { // wait until the document is ready
-  $('div#chatroom').click(function(){
+  $(".btn-success.ceremony").click(function(){
+    var invite_id = $(".btn-success.ceremony").data().inviteId;
     $.ajax({
-      type: 'GET',
-      url: 'chatget.php',
-      data: { chatroomid: '<?php echo $chatroomid; ?>'},
-      datatype: 'html',
+      type: 'POST',
+      url: '/invites/' + invite_id + '.json',
+      data: { _method: "PUT", invite: { attending_ceremony: 1 } },
+      datatype: 'json',
       cache: 'false',
       success: function(response) {
-          $('#chatroom').append(response);
-          alert('Load was performed.');
+        location.reload();
       },
       error: function(){
-          alert('Fuuuuuuuuuuuuuu');
+        alert("There was an error - a group of trained monkeys will hopefully fix it soon. If they don't make it - please send us a message on Facebook");
       }
-    }); // End Ajax  
-  alert('Fail');
-  }); // End onclick
+    });
+  });
+  
+  $(".btn-link.danger.ceremony").click(function(){
+    var invite_id = $(".btn-link.danger.ceremony").data().inviteId;
+    $.ajax({
+      type: 'POST',
+      url: '/invites/' + invite_id + '.json',
+      data: { _method: "PUT", invite: { attending_ceremony: 2 } },
+      datatype: 'json',
+      cache: 'false',
+      success: function(response) {
+        location.reload();
+      },
+      error: function(){
+        alert("There was an error - a group of trained monkeys will hopefully fix it soon. If they don't make it - please send us a message on Facebook");
+      }
+    });
+  });
+  
+  $(".btn-success.reception").click(function(){
+    var invite_id = $(".btn-success.reception").data().inviteId;
+    $.ajax({
+      type: 'POST',
+      url: '/invites/' + invite_id + '.json',
+      data: { _method: "PUT", invite: { attending_reception: 1 } },
+      datatype: 'json',
+      cache: 'false',
+      success: function(response) {
+        location.reload();
+      },
+      error: function(){
+        alert("There was an error - a group of trained monkeys will hopefully fix it soon. If they don't make it - please send us a message on Facebook");
+      }
+    });
+  });
+  
+  $(".btn-link.danger.reception").click(function(){
+    var invite_id = $(".btn-link.danger.reception").data().inviteId;
+    $.ajax({
+      type: 'POST',
+      url: '/invites/' + invite_id + '.json',
+      data: { _method: "PUT", invite: { attending_reception: 2 } },
+      datatype: 'json',
+      cache: 'false',
+      success: function(response) {
+        location.reload();
+      },
+      error: function(){
+        alert("There was an error - a group of trained monkeys will hopefully fix it soon. If they don't make it - please send us a message on Facebook");
+      }
+    });
+  });
 });
 
