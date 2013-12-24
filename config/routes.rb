ba9116a3a04d1cff828dab28b49a5d92c60c01ab
Mark Cracknell <mark@kondoot.com>
2013-12-24 22:46:02 +1000

    A wide range of changes - mostly to do with content and styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Wedding::Application.routes.draw do
   resources :invites
   
   get 'welcome'           => 'pages#welcome', :as => :welcome
-  get 'our_story'         => 'pages#our_story', :as => :our_story
   get 'recommendations'   => 'pages#recommendations', :as => :recommendations
   get 'locations'         => 'pages#locations', :as => :locations  
   
-  get 'registry' => redirect("http://google.com/"), :as => :registry
+  get 'registry' => redirect("https://www.notanothertoaster.com/natv3/login.jsp?username=UTRIRA&password=712542"), :as => :registry
   get '*path' => 'invites#show'
   
   root 'pages#welcome'
