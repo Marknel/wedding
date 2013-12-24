6c3521e6c9ebbca6690055e2b30a78817121b245
Mark Cracknell <mark@kondoot.com>
2013-12-24 15:37:40 +1000

    Put the secret token back in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wedding::Application.config.secret_key_base = 'dbdaa4836b7a12dd832a6e1a03eeb34da4feca88b0823d16952fcdbcce6cc22fdef49742433584794259fc2688967e99a316b0a28072240a254e6514800b5c93'
